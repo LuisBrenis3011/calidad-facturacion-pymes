19e479bc8654143ef43e97910ed1025165f88e47
brenis <angelvir96@gmail.com>
2025-09-27 12:47:09 -0500

    ambiente de desarrollo inicial

---
+++ src/main/java/com/billtel/calidad/facturacion_pymes/FacturacionPymesApplication.java
@@ -0,0 +1,13 @@
+package com.billtel.calidad.facturacion_pymes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionPymesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionPymesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/billtel/calidad/facturacion_mypes/FacturacionMypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billtel.calidad.facturacion_mypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturacionMypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
