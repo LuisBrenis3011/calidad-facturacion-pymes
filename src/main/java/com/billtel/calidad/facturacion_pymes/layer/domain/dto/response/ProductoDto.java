496becff51e0c25828f89892cc20cb8f001276b1
brenis <angelvir96@gmail.com>
2025-11-01 13:35:21 -0500

    faltan las pruebas para services y facades

--- src/main/java/com/billtel/calidad/facturacion_pymes/layer/domain/dto/response/ProductoDto.java
+++ src/main/java/com/billtel/calidad/facturacion_pymes/layer/domain/dto/response/ProductoDto.java
@@ -6,10 +6,12 @@ import lombok.*;
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class ProductoDto {
     private Long id;
     private String nombre;
     private String descripcion;
     private BigDecimal valorUnitario;
     private String unidadMedida;
+
 }
--- src/test/java/com/billtel/calidad/facturacion_pymes/FacturacionPymesApplicationTests.java
+++ src/test/java/com/billtel/calidad/facturacion_pymes/FacturacionPymesApplicationTests.java
@@ -1,9 +1,11 @@
 package com.billtel.calidad.facturacion_pymes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class FacturacionPymesApplicationTests {
 
 	@Test
